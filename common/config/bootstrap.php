10a4c1d31b11c0b58dbcf4f17ee6e781d87e9e39
ligang <2144713621@qq.com>
2016-05-27 11:41:46 +0800

    gold framework submit

---
+++ common/config/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('@admin', dirname(dirname(__DIR__)) . '/admin');
+Yii::setAlias('@chair', dirname(dirname(__DIR__)) . '/chair');
+Yii::setAlias('@seller', dirname(dirname(__DIR__)) . '/seller');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
